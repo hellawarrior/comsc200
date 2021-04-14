ce7e91229f7e5006a65c9ad978f1eb193cfeb114
Shawn Yang <32279027+hellawarrior@users.noreply.github.com>
2021-04-14 10:34:47 -0700

    Add files via upload

---
+++ elevator2.1/Elevator.h
@@ -0,0 +1,60 @@
+#ifndef ELEVATOR_H
+#define ELEVATOR_H
+
+#include "Panel.h"
+#include "Rider.h"
+#include "Floor.h"
+#include "Building.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+class Elevator
+{
+	static const int IDLE = 0;
+	static const int UP = 1;
+	static const int DOWN = -1;
+
+	// elevator metrics
+	
+	int location; // inches above ground level
+	int direction; // equal to IDLE, UP, DOWN
+	int atFloorIndex; // -1 if door is closed
+	int timer;
+
+	vector<Rider> riders;
+
+public:
+	const int speed;//inches per second, up or down
+	const unsigned int CAPACITY;
+	Elevator(unsigned int, int, int);
+	operator int() const { return location; }
+
+	// count-down timer before closing door
+	void resetTimer() { timer = 3; }
+	void tickTimer() { timer--; }
+	bool timedOut() const { return timer == 0; }
+
+	bool isOpen() const { return atFloorIndex >= 0; }
+	void openDoorTo(int); // parameter is index in Building::floors array
+	void board(const Rider&); // adds a Rider to the Elevator
+
+	// added in lab 9
+	bool hasRiderForFloor() const;
+	void removeRider();
+	bool isFull() const { return CAPACITY == riders.size(); }
+	bool goingUp() const { return direction == UP; }
+	bool goingDown() const { return direction == DOWN; }
+	int getFloorIndex() const { return atFloorIndex; }
+	unsigned int getNumberOfRiders() const { return riders.size(); }
+	
+	Panel panel;
+
+	// friend function
+	friend ostream& operator<<(ostream&, const Elevator&);
+};
+
+#endif#pragma once
